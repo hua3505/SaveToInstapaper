e585dc67c761a7868ce266929f2307a0d7636608
wolf <959547664@qq.com>
2017-01-27 11:26:41 +0800

    first commit: finish function of saving to instapaper

---
+++ app/src/main/java/com/gmail/huashadow/sharetoinstapaper/ShareActivity.java
@@ -0,0 +1,39 @@
+package com.gmail.huashadow.sharetoinstapaper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ShareActivity extends Activity {
+
+    public static final String TAG = ShareActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        saveToInstapaper();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.d(TAG, "onNewIntent");
+        super.onNewIntent(intent);
+        saveToInstapaper();
+    }
+
+    private void saveToInstapaper() {
+        Log.d(TAG, "saveToInstapaper");
+        Intent intent = getIntent();
+        if (intent.getAction().equals(Intent.ACTION_VIEW)) {
+            Log.d(TAG, intent.getData().toString());
+            Intent saveToInstapaperIntent = new Intent(Intent.ACTION_SEND);
+            saveToInstapaperIntent.setPackage("com.instapaper.android");
+            saveToInstapaperIntent.setType("text/plain");
+            saveToInstapaperIntent.putExtra(Intent.EXTRA_TEXT, intent.getData().toString());
+            startActivity(saveToInstapaperIntent);
+        }
+        finish();
+    }
+}
